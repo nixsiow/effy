8d25af8c92cbdbf214b0a67306607a0751062fa1
nixsiow <nixsiow@hotmail.com>
2014-03-28 15:53:06 +1100

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,9 @@ class User < ActiveRecord::Base
     has_many :tasks
 
     has_secure_password
+    # validates :name, :presence => true, length: { minimum: 6, maximum: 20 }, :uniqueness => { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows alphanumeric characters" }
+    # validates :password, :presence => true, length: { minimum: 6, maximum: 20 }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows alphanumeric characters" }
     validates :image, :presence => true
-    validates :name, :presence => true, length: { minimum: 6, maximum: 20 }, :uniqueness => { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows alphanumeric characters" }
-    validates :password, :presence => true, length: { minimum: 6, maximum: 20 }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows alphanumeric characters" }
+    validates :name, :presence => true, :length => { :minimum => 2 }
+    validates :name, :uniqueness => true
 end
