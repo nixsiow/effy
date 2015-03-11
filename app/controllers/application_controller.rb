09cbcc4b7da0f3451383c429121f574a93a4fd61
Nix Siow <nixsiow@hotmail.com>
2015-03-11 23:19:39 +0800

    bug detected from find_weather, modified. first attempt to push heroku.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    
+
     before_filter :authenticate
     before_filter :find_weather
 
@@ -14,23 +14,39 @@ class ApplicationController < ActionController::Base
     end
 
     def find_weather
-        if (session[:weather].nil? || Time.now - session[:last_weather_lookup] > (30 * 60))
+      # raise params.inspect
+        if (session[:weather].nil? || Time.now - session[:last_weather_lookup] > (15 * 60))
             ip = request.remote_ip
             if (ip == '127.0.0.1')
-                ip = '202.171.180.162'
+                ip = '175.142.143.147'
             end
             location = Geocoder.search(ip).first
             session[:location] = "#{location.city}, #{location.country}"
             weather = OpenWeather::Current.city(session[:location])
-            session[:weather] = weather['weather'][0]['id']
+
+            if (weather['weather'] == nil)
+              session[:weather] = 800
+            else
+              session[:weather] = weather['weather'][0]['id']
+            end
+
             session[:last_weather_lookup] = Time.now
 
-            # get celcius
-            kelvin = weather['main']['temp']
+            # get celcius(fake if openweather API unable to get the weather info based on the ip)
+            if (weather['weather'] == nil)
+              kelvin = 300.25
+            else
+              kelvin = weather['main']['temp']
+            end
+
             session[:celcius] = (kelvin - 273.15).round
 
-            # get description
-            session[:description] = weather['weather'].first['description'].capitalize
+            # get description(fake if openweather API unable to get the weather info based on the ip)
+            if (weather['weather'] == nil)
+              session[:description] = "Sky is Clear"
+            else
+              session[:description] = weather['weather'].first['description'].capitalize
+            end
         end
     end
 end
